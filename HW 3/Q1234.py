3cfaa1e89506480aef5ac9a1917c048a7c6cc251
whz20024117 <whz20024117@gmail.com>
2020-09-28 11:40:04 -0400

    HW 3 Q3 Done

--- HW 3/Q1234.py
+++ HW 3/Q1234.py
@@ -3,11 +3,14 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from xgboost import XGBClassifier
 from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import roc_auc_score
+from imblearn.over_sampling import SMOTE
 
 
 
@@ -43,6 +46,33 @@ def _auc_training_process(clf_class, X, y, **kwargs):
         test_X, test_y = X[i_test], y[i_test]
 
         clf = clf_class(**kwargs)
+        clf.fit(train_X, train_y)
+
+        test_auc = roc_auc_score(test_y, clf.predict_proba(test_X)[:, 1])
+        auc_scores.append(test_auc)
+
+    return auc_scores
+
+
+def _svm_training_process(X, y, C):
+    """
+    :param clf_class: sklearn-style classifier class
+    :param X: training data
+    :param y: label
+    :param kwargs: arguments pass to classifier class __init__
+    :return: list: rain_auc_scores, list: auc_scores
+    """
+
+    # 5 fold cv
+    kf = KFold(n_splits=5)
+
+    auc_scores = []
+
+    for i_train, i_test in kf.split(X):
+        train_X, train_y = X[i_train], y[i_train]
+        test_X, test_y = X[i_test], y[i_test]
+
+        clf = make_pipeline(StandardScaler(), SVC(C=C, probability=True))
         clf.fit(train_X, train_y)
 
         test_auc = roc_auc_score(test_y, clf.predict_proba(test_X)[:, 1])
@@ -91,14 +121,15 @@ def q2():
     X, y = _load_shuffle_data('./data/credit_card_train.csv')
 
     # Random forest---------------------------------------------------------------------------------
-    n_estimator_list = [50, 100, 200, 300, 500]
+    n_estimator_list = [100, 200, 300, 450]
     score_list = []
 
     print("RandomForest Results: ")
     for p in n_estimator_list:
         print("    {} is: {}".format('n_estimators', p))
         auc_scores = _auc_training_process(RandomForestClassifier, X, y,
-                                           n_estimators=p)
+                                           n_estimators=p,
+                                           n_jobs=-1)
         _print_results(auc_scores)
         score_list.append(auc_scores)
 
@@ -128,8 +159,7 @@ def q2():
     print("SVM Results: ")
     for p in c_list:
         print("    {} is: {}".format('C', p))
-        auc_scores = _auc_training_process(SVC, X, y,
-                                           C=p)
+        auc_scores = _svm_training_process(X, y, p)
 
         _print_results(auc_scores)
         score_list.append(auc_scores)
@@ -165,8 +195,94 @@ def q2():
     print("\n")
 
 
+def q3():
+    X, y = _load_shuffle_data('./data/credit_card_train.csv')
+    print("Number of data samples: {}".format(len(X)))
+    sampler = SMOTE(random_state=0)
+    X, y = sampler.fit_resample(X, y)
+    print("Number of data samples after resample: {}".format(len(X)))
+    # Shuffle Data
+    np.random.seed(10)
+    idx = np.random.permutation(range(len(X)))
+    X, y = X[idx], y[idx]
+
+    # # Random forest---------------------------------------------------------------------------------
+    # n_estimator_list = [100, 200, 300, 450]
+    # score_list = []
+    #
+    # print("RandomForest Results: ")
+    # for p in n_estimator_list:
+    #     print("    {} is: {}".format('n_estimators', p))
+    #     auc_scores = _auc_training_process(RandomForestClassifier, X, y,
+    #                                        n_estimators=p,
+    #                                        n_jobs=-1)
+    #     _print_results(auc_scores)
+    #     score_list.append(auc_scores)
+    #
+    # _save_results('./results/Q3_RandomForest.csv', score_list, p_name='n_estimators', p=n_estimator_list)
+    # print("\n")
+    #
+    # # XGBOOST--------------------------------------------------------------------------------------------
+    # learning_rate_list = [0.01, 0.1, 0.25]
+    # score_list = []
+    #
+    # print("XGBoost Results: ")
+    # for p in learning_rate_list:
+    #     print("    {} is: {}".format('learning_rate', p))
+    #     auc_scores = _auc_training_process(XGBClassifier, X, y,
+    #                                        learning_rate=p)
+    #
+    #     _print_results(auc_scores)
+    #     score_list.append(auc_scores)
+    #
+    # _save_results('./results/Q3_xgb.csv', score_list, p_name='learning_rate', p=learning_rate_list)
+    # print("\n")
+
+    # SVM-----------------------------------------------------------------------------------------------
+    c_list = [0.1, 1.0, 10.0]
+    score_list = []
+
+    print("SVM Results: ")
+    for p in c_list:
+        print("    {} is: {}".format('C', p))
+        auc_scores = _svm_training_process(X[:50000], y[:50000], p)  # Downsampled due to high computation cost on svm
+
+        _print_results(auc_scores)
+        score_list.append(auc_scores)
+
+    _save_results('./results/Q2_SVM.csv', score_list, p_name='C', p=c_list)
+    print("\n")
+
+    # KNN--------------------------------------------------------------------------------------
+    n_neighbors_list = [3, 5, 7, 9]
+    score_list = []
+
+    print("KNN Results: ")
+    for p in n_neighbors_list:
+        print("    {} is: {}".format('n_neighbors', p))
+        auc_scores = _auc_training_process(KNeighborsClassifier, X, y,
+                                           n_neighbors=p)
+
+        _print_results(auc_scores)
+        score_list.append(auc_scores)
+
+    _save_results('./results/Q3_KNN.csv', score_list, p_name='n_neighbors', p=n_neighbors_list)
+    print("\n")
+
+    # Naive Bayes----------------------------------------------------------------------------------
+    score_list = []
+
+    print("Naive Bayes Results: ")
+    auc_scores = _auc_training_process(GaussianNB, X, y)
+    score_list.append(auc_scores)
+
+    _print_results(auc_scores)
+    _save_results('./results/Q3_NB.csv', score_list)
+    print("\n")
+
 
 if __name__ == '__main__':
-    #q1()
-    q2()
-    exit()
+    # q1()
+    # q2()
+    q3()
+
